94b4943e498cd8363158021bf08e8d5898e454d8
Talha Kerpicci <44553433+N1ght-Fury@users.noreply.github.com>
2019-09-04 21:33:01 +0300

    Update house_database.py

--- house_database.py
+++ house_database.py
@@ -32,14 +32,25 @@ class Database_Post():
                 "Date text," \
                 "Neighborhood text," \
                 "Room text," \
-                "Img text)"
-        self.cursor.execute(query)
+                "Img text);" \
+                "create table if not exists " \
+                "Tbl_Posts_Backup (" \
+                "Link text," \
+                "Title text," \
+                "Price text," \
+                "M2 text," \
+                "Date text," \
+                "Neighborhood text," \
+                "Room text," \
+                "Img text);" \
+
+        self.cursor.executescript(query)
         self.connection.commit()
 
-    def check_if_house_exists(self,link,img):
+    def check_if_house_exists(self,link):
 
-        query = "select * from Tbl_Posts where Link = @p1 or Img = @p2"
-        self.cursor.execute(query,(link,img,))
+        query = "select * from Tbl_Posts where Link = @p1"
+        self.cursor.execute(query,(link,))
         posts = self.cursor.fetchall()
 
         if (len(posts) == 0):
@@ -48,8 +59,11 @@ class Database_Post():
         else:
             return 1
 
-    def add_house(self,House):
+    def add_house(self,House,backup=False):
 
-        query = "insert into Tbl_Posts values (@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8)"
+        if (backup):
+            query = "insert into Tbl_Posts_Backup values (@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8)"
+        else:
+            query = "insert into Tbl_Posts values (@p1,@p2,@p3,@p4,@p5,@p6,@p7,@p8)"
         self.cursor.execute(query,(House.house_link,House.title,House.price,House.m2,House.date,House.neighborhood,House.room,House.img))
         self.connection.commit()
